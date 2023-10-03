d9b0c3c5ad6f8e3e2a774e5dc4d08bae2c5786f1
tanmayshell <146022160+tanmayshell@users.noreply.github.com>
2023-10-03 12:53:52 +0530

    added comments

--- xmlToPdf.js
+++ xmlToPdf.js
@@ -6,6 +6,8 @@ import { parseString } from 'xml2js'
 async function loadpartials(){
     try{
         const header=await fs.readFile('header.hbs', 'utf-8')
+
+        //Register partial
         handlebars.registerPartial('header', header)
     } catch(error){
         console.error('Can not load partials')
@@ -14,6 +16,8 @@ async function loadpartials(){
 
 async function convertXmlToPdf(){
     try{
+
+        //Read XML data
         const data=await fs.readFile('task.xml', 'utf-8')
         async function parseXml(data){
             return new Promise((resolve, reject)=>{
@@ -26,11 +30,22 @@ async function convertXmlToPdf(){
                 })
             })
         }
+
+        // Parse XML data to create JS object
         const jsObject= await parseXml(data)
+        
+        //Load Partials
         await loadpartials()
+
+        // Read Handlebar Template
         const template=await fs.readFile('task.hbs', 'utf-8')
+
         console.log(jsObject)
+
+        // Compile template
         const compiledTemplate= handlebars.compile(template)
+
+        // Define input context
         const inputs={
             title: 'Movies and Series',
             header: 'Popular Movies and Series',
@@ -40,12 +55,20 @@ async function convertXmlToPdf(){
 
         }
         
+        //Generate HTML content
         const html=compiledTemplate(inputs)
+
+        //Launch Puppeteer
         const browser= await puppeteer.launch()
         const page= await browser.newPage()
 
+        // Set HTML content of the page
         await page.setContent(html)
+
+        //Generate PDF
         await page.pdf({path: 'output.pdf', format: 'A4'})
+
+        //Close the browser
         await browser.close()
 
     } catch(error){
